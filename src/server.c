3a29c1b43efc28116a09bafe029a108fd468d263
Amaury Denoyelle <adenoyelle@haproxy.com>
2026-09-10 11:51:22 +0200

    MEDIUM: server: implement "nofail" mode for CLI server deletion
    Implement an alternative mode for server deletion via CLI "del server"
    command. This is active when using the new extra argument "nofail".
    
    The objective of this mode is to simplify server deletion by limiting
    the failure cases. First, it automatically put the server in maintenance
    and disable agent checks if needed. In case the server cannot be deleted
    due to active/idle/queued connections, flag SRV_F_TO_DELETE is set on
    the server. Users only have to use "wait srv-deleted" which will
    finalize server deletion asap.
    
    SRV_F_TO_DELETE is resetted each time the server state is set. This
    operation can be performed via several methods (CLI, stats web
    interface, agent checks, ...). To ensure every paths are covered, flag
    is removed directly in srv_adm_set_*() functions. This is the case even
    if the server remains in maintenance mode.

--- src/server.c
+++ src/server.c
@@ -7017,12 +7017,22 @@ static int cli_parse_delete_server(char **args, char *payload, struct appctx *ap
 {
 	struct server *srv;
 	const char *msg;
-	int ret;
+	int ret, nofail = 0;
 
 	if (!cli_has_level(appctx, ACCESS_LVL_ADMIN))
 		return 1;
 
 	++args;
+
+	if (*args[2]) {
+		if (strcmp(args[2], "nofail") == 0) {
+			nofail = 1;
+		}
+		else {
+			cli_err(appctx, "Invalid extra argument.\n");
+			goto out;
+		}
+	}
 
 	/* The proxy servers list is currently not protected by a lock so this
 	 * requires thread isolation. In addition, any place referencing the
@@ -7038,10 +7048,22 @@ static int cli_parse_delete_server(char **args, char *payload, struct appctx *ap
 		goto out;
 	}
 
+	if (nofail &&
+	    (!(srv->cur_admin & SRV_ADMF_MAINT) || srv->agent.state & CHK_ST_ENABLED)) {
+		srv_adm_set_maint(srv);
+		srv->agent.state &= ~CHK_ST_ENABLED;
+	}
+
 	ret = srv_check_for_deletion(srv, &msg);
 	if (ret <= 0) {
-		/* failure (recoverable or not) */
-		cli_err(appctx, msg);
+		if (!ret && nofail) {
+			srv->flags |= SRV_F_TO_DELETE;
+			cli_msg(appctx, LOG_INFO, "Server flagged for deletion.\n");
+		}
+		else {
+			/* failure (recoverable or not) */
+			cli_err(appctx, msg);
+		}
 		goto out;
 	}
 
